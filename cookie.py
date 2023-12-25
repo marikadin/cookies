36f15501d0932b4fff43472f06aad93d38402c04
marikadin <153016365+marikadin@users.noreply.github.com>
2023-12-25 14:23:29 +0200

    Update cookie.py

--- cookie.py
+++ cookie.py
@@ -20,13 +20,13 @@ if 'clicked' not in st.session_state:
 
 def click_button():
     st.session_state.clicked = True
-
 def main():
     st.title("Google Login with Streamlit")
 
+    # Add a button to initiate the login process
     st.button('Analyze', on_click=click_button)
     if st.session_state.clicked:
-
+        # Display a Google Sign-In button, allowing the user to sign in with their Google account
         login_url = f'https://accounts.google.com/o/oauth2/auth?client_id={CLIENT_ID}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=openid%20profile%20email&response_type=code'
         st.markdown(f"[Sign in with Google]({login_url})")
 
@@ -34,7 +34,7 @@ def main():
         # Retrieve the code from the user input
         code = st.text_input("Enter the code from the redirected URL:")
 
-        # Exchange the code for an ID token (perform this securely on the server side)
+        # Exchange the code for an ID token
         if code:
             id_token_string = exchange_code_for_id_token(code)
             if id_token_string:
@@ -44,12 +44,18 @@ def main():
 
                     # Save the user's email in Streamlit secrets
                     st.secrets["user_email"] = user_info['email']
-                    st.write(user_info)
+
 
 def exchange_code_for_id_token(code):
-    # In a real-world scenario, you should exchange the code for an ID token securely on the server side
+    # Placeholder for the ID token (replace this with your actual implementation)
+    # In a real application, use a secure method to exchange the code for an ID token
     # This might involve making a request to your server, which then communicates with Google's OAuth endpoint
-    st.warning(user_info[0])
+    # to exchange the code for an ID token
+    st.warning("In a real-world scenario, you should exchange the code for an ID token securely on the server side.")
+
+    # Placeholder for the ID token (replace this with your actual implementation)
+    return "your-id-token"
+
 
 if __name__ == "__main__":
     main()
