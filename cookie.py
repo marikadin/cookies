f2c5033ac6a127e025ccd0dec0d37cd31f6bb36d
marikadin <153016365+marikadin@users.noreply.github.com>
2023-12-26 13:57:25 +0200

    dh

--- cookie.py
+++ cookie.py
@@ -1,5 +1,47 @@
 import streamlit as st
+import json
 
-name = st.text_input(" please enter username")
-password = st.text_input(" please enter username")
+# Function to read the JSON file
+def load_data():
+    try:
+        with open("secrets.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+    return data
 
+# Function to save data to the JSON file
+def save_data(data):
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent=2)
+
+# Streamlit app
+def main():
+    st.title("Streamlit JSON Editor")
+
+    # Load data from the JSON file
+    data = load_data()
+
+    # Display current data
+    st.subheader("Current Data:")
+    st.write(data)
+
+    # Input new key-value pair
+    key = st.text_input("Enter Key:")
+    value = st.text_input("Enter Value:")
+
+    # Add the new key-value pair to the data
+    if st.button("Add to Dictionary"):
+        if key and value:
+            data[key] = value
+            st.success(f"Added: {key}: {value}")
+            save_data(data)
+        else:
+            st.warning("Please enter both key and value.")
+
+    # Display updated data
+    st.subheader("Updated Data:")
+    st.write(data)
+
+if __name__ == "__main__":
+    main()
